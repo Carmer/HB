a046b8425385fcc9c329a52a6ec6f527f4786ed5
Andrew Carmer <carmerandrew@gmail.com>
2015-07-05 13:48:42 -0600

    fix dumb mmistake with bracket in wrong place

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV["sendgrid_username"],
-  :password => ENV[]"sendgrid_password"],
+  :password => ENV["sendgrid_password"],
   :domain => "hipster-bytes.herokuapp.com",
   :address => "smtp.sendgrid.net",
   :port => 587,
